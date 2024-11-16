26a08a3a8374176f2b556d78aa4b05a8dce2dfb6
Anna Carolinne Albuquerque de Medeiros <70245734+anna-albuquerque@users.noreply.github.com>
2024-11-16 20:26:52 +0000

    Fix: corrigindo Urls.py

--- Backend/nanny_pets_app/urls.py
+++ Backend/nanny_pets_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import ( 
+from .views import (
     CaracteristicasAPIView,
     CaracteristicasDoCuidadorView,
     CuidadorAPIView,
@@ -14,8 +14,8 @@ urlpatterns = [
     path('cuidadores/', CuidadorAPIView.as_view(), name='cuidadores'),
     path('caracteristicas/', CaracteristicasAPIView.as_view(), name='caracteristicas'),
     path('tutores/',TutorAPIView.as_view(), name='tutores'),
-    path('cuidadores/<int:cuidador_id>/caracteristicas/', 
-         CaracteristicasDoCuidadorView.as_view(), 
+    path('cuidadores/<int:cuidador_id>/caracteristicas/',
+         CaracteristicasDoCuidadorView.as_view(),
          name='caracteristicascuidador'),
 
     path('cuidadores-filtrados/', CuidadorFiltradoView.as_view(), name='cuidadores-filtrados'),
