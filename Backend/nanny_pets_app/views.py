ec74fb95e3136d3c4fcfad29db8864ddbad12dd8
Anna Carolinne Albuquerque de Medeiros <70245734+anna-albuquerque@users.noreply.github.com>
2024-11-16 20:02:17 +0000

    Fix: views.py

--- Backend/nanny_pets_app/views.py
+++ Backend/nanny_pets_app/views.py
@@ -1,57 +1,59 @@
+"""
+Este módulo contém as views relacionadas ao gerenciamento de cuidadores, tutores e características.
+"""
+
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, generics
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
-from .serializers import UserSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from django.contrib.auth.models import User  # Importando modelo User
+from .serializers import (
+    UserSerializer,
+    TutorSerializer,
+    CuidadorSerializer,
+    CaracteristicasSerializer
+)
 from .models import Cuidador, Caracteristicas, Tutor
-from .serializers import TutorSerializer, CuidadorSerializer, CaracteristicasSerializer
-from django_filters.rest_framework import DjangoFilterBackend  # Importando filtro mais robusto
-from django.contrib.auth.models import User
 
 class CustomAuthToken(ObtainAuthToken):
-# View para autenticação personalizada de usuários utilizando tokens. #
+    """View para autenticação personalizada de usuários utilizando tokens."""
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
-        token, created = Token.objects.get_or_create(user=user)
+        token, _ = Token.objects.get_or_create(user=user)
         return Response({
             'token': token.key,
             'user_id': user.pk,
-            'email': user.email
+            'email': user.email,
         })
 
 class UserRegisterView(APIView):
+    """View para registrar novos usuários."""
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
-            # Verificação se o usuário já existe
             if User.objects.filter(email=request.data.get('email')).exists():
                 return Response({'detail': 'User with this email already exists.'}, 
-                                tatus=status.HTTP_400_BAD_REQUEST)
+                                status=status.HTTP_400_BAD_REQUEST)
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class CuidadorFiltradoView(generics.ListAPIView):
+    """View para listar cuidadores com filtro por características."""
     serializer_class = CuidadorSerializer
     queryset = Cuidador.objects.all()
-    filter_backends = [DjangoFilterBackend]  # Filtro mais robusto
-    filterset_fields = ['caracteristicas']  # Filtro para o campo 'caracteristicas'
-
-    def filter_queryset(self, queryset):
-        caracteristicas = self.request.query_params.getlist('caracteristicas')
-        if caracteristicas:
-            queryset = queryset.filter(caracteristicas__id__in=caracteristicas).distinct()
-        return queryset
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['caracteristicas']
 
 class CuidadorAPIView(APIView):
-    serializer_class = CuidadorSerializer
-
+    """View para criar e listar cuidadores."""
     def post(self, request):
-        serializer = self.serializer_class(data=request.data)
+        serializer = CuidadorSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -59,7 +61,6 @@ class CuidadorAPIView(APIView):
 
     def get(self, request):
         caracteristicas = request.query_params.getlist('caracteristicas')
-        # Filtro diretamente na consulta do banco de dados
         cuidadores = Cuidador.objects.all()
         if caracteristicas:
             cuidadores = cuidadores.filter(caracteristicas__id__in=caracteristicas).distinct()
@@ -67,12 +68,14 @@ class CuidadorAPIView(APIView):
         return Response(serializer.data)
 
 class CaracteristicasAPIView(APIView):
+    """View para listar todas as características."""
     def get(self, request):
         caracteristicas = Caracteristicas.objects.all()
         serializer = CaracteristicasSerializer(caracteristicas, many=True)
         return Response(serializer.data)
 
 class CaracteristicasDoCuidadorView(APIView):
+    """View para listar as características de um cuidador específico."""
     def get(self, request, cuidador_id):
         cuidador = get_object_or_404(Cuidador, id=cuidador_id)
         caracteristicas = cuidador.caracteristicas.all()
@@ -80,18 +83,16 @@ class CaracteristicasDoCuidadorView(APIView):
         return Response(serializer.data)
 
 class TutorAPIView(APIView):
-    serializer_class = TutorSerializer
-
+    """View para criar e listar tutores."""
     def get(self, request):
         tutores = Tutor.objects.all()
         serializer = TutorSerializer(tutores, many=True)
         return Response(serializer.data)
 
     def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-
+        serializer = TutorSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
