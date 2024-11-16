59bfe9317ca4d2a5c7cc97a048f6b4e735b671b5
Anna Carolinne Albuquerque de Medeiros <70245734+anna-albuquerque@users.noreply.github.com>
2024-11-16 23:46:14 +0000

    Fix: correção

--- Backend/nanny_pets_app/views.py
+++ Backend/nanny_pets_app/views.py
@@ -3,11 +3,11 @@ Este módulo contém as views relacionadas ao gerenciamento de cuidadores, tutor
 """
 
 from django.shortcuts import get_object_or_404
+from rest_framework.authtoken.models import Token
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, generics
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.authtoken.models import Token
 from django_filters.rest_framework import DjangoFilterBackend
 from django.contrib.auth.models import User  # Importando modelo User
 from .serializers import (
