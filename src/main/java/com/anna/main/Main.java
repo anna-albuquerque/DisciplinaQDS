f5bce77388923bca2b433ed906061b7cb6a3d278
Anna Carolinne Albuquerque de Medeiros <70245734+anna-albuquerque@users.noreply.github.com>
2024-12-11 02:34:03 +0000

    Fix: main

--- src/main/java/com/anna/main/Main.java
+++ src/main/java/com/anna/main/Main.java
@@ -4,6 +4,8 @@ import com.anna.controller.PaymentController;
 import com.anna.controller.ProductController;
 import java.util.Date;
 import java.util.Date;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
