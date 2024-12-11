ada27b88a5decba616e9333c36b9a257b4c63daa
Anna Carolinne Albuquerque de Medeiros <70245734+anna-albuquerque@users.noreply.github.com>
2024-12-11 02:32:17 +0000

    Fix: pom

--- src/main/java/com/anna/main/Main.java
+++ src/main/java/com/anna/main/Main.java
@@ -8,7 +8,7 @@ import java.util.Date;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(ExampleApplication.class, args);
+        SpringApplication.run(Main.class, args);
     }
 
     //public static void main(String[] args) {
