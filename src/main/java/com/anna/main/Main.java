5308144f3752629c80b6d6230145e10cce1dd544
Anna Carolinne Albuquerque de Medeiros <70245734+anna-albuquerque@users.noreply.github.com>
2024-12-16 11:20:33 +0000

    :bug: Fix: PMD Failure: com.anna.service.Main:4 Rule:UnnecessaryImport Priority:4 Unused import.

--- src/main/java/com/anna/main/Main.java
+++ src/main/java/com/anna/main/Main.java
@@ -1,10 +1,5 @@
 package com.anna.service;
 
-import com.anna.controller.CrudController;
-import com.anna.controller.PaymentController;
-import com.anna.controller.ProductController;
-import java.util.Date;
-import java.util.Date;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
