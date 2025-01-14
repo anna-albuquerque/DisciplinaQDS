df7ca8c725a695b66d595284aed23337470a3f57
Anna Carolinne Albuquerque de Medeiros <70245734+anna-albuquerque@users.noreply.github.com>
2025-01-14 12:41:53 +0000

    Fix: sonarCloud CustomerDTO

--- src/main/java/com/anna/domain/CustomerDTO.java
+++ src/main/java/com/anna/domain/CustomerDTO.java
@@ -1,5 +1,45 @@
 package com.anna.domain;
 
 public class CustomerDTO {
-    
+
+    private String name;
+    private String email;
+    private String address;
+
+    // Construtor
+    public CustomerDTO(String name, String email, String address) {
+        this.name = name;
+        this.email = email;
+        this.address = address;
+    }
+
+    // Getters e setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerDTO [name=" + name + ", email=" + email + ", address=" + address + "]";
+    }
 }
