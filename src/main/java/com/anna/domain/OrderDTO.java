80150ba5ee8e739b8100bac72fbc76b0440f52ce
Anna Carolinne Albuquerque de Medeiros <70245734+anna-albuquerque@users.noreply.github.com>
2025-01-14 12:42:35 +0000

    Fix: sonarCloud OrderDTO

--- src/main/java/com/anna/domain/OrderDTO.java
+++ src/main/java/com/anna/domain/OrderDTO.java
@@ -1,4 +1,58 @@
-package com.anna.domain;
+package com.anna.DTO;
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 public class OrderDTO {
+
+    private int orderId;
+    private String customerName;
+    private Date orderDate;
+    private BigDecimal totalAmount;
+
+    // Construtor
+    public OrderDTO(int orderId, String customerName, Date orderDate, BigDecimal totalAmount) {
+        this.orderId = orderId;
+        this.customerName = customerName;
+        this.orderDate = orderDate;
+        this.totalAmount = totalAmount;
+    }
+
+    // Getters e Setters
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public BigDecimal getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(BigDecimal totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderDTO [orderId=" + orderId + ", customerName=" + customerName + ", orderDate=" + orderDate + ", totalAmount=" + totalAmount + "]";
+    }
 }
