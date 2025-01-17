5ecdc9dd0ae0b86d4c0ce9e9790bb32b9114879f
Anna Carolinne Albuquerque de Medeiros <70245734+anna-albuquerque@users.noreply.github.com>
2025-01-17 10:55:31 +0000

    Fix: corrigindo OrderDTO

--- src/main/java/com/anna/domain/CustomerDTO.java
+++ src/main/java/com/anna/domain/CustomerDTO.java
@@ -1,4 +1,5 @@
 package com.anna.domain;
 
 public class CustomerDTO {
+    
 }
--- src/main/java/com/anna/domain/OrderDTO.java
+++ src/main/java/com/anna/domain/OrderDTO.java
@@ -1,7 +1,5 @@
 package com.anna.domain;
 
-import java.math.BigDecimal;
-import java.util.Date;
-
 public class OrderDTO {
+
 }
